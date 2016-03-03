a701fbcd83d0e92d5d25f5ca07323bb0b6127f97
lude-ma <ludersdorfer@fortiss.org>
2016-03-03 15:24:18 +0100

    documenting

--- nodes/turtle_tf_broadcaster.py
+++ nodes/turtle_tf_broadcaster.py
@@ -7,8 +7,12 @@ import turtlesim.msg
 
 def handle_turtle_pose(msg, turtlename):
     br = tf.TransformBroadcaster()
+    # Publish this turtle's translation and rotation
+    # as a translation from frame 'world' to frame'turtlename'.
     br.sendTransform((msg.x, msg.y, 0),
-                     tf.transformations.quaternion_from_euler(0, 0, msg.theta),
+                     tf.transformations.quaternion_from_euler(0,
+                                                              0,
+                                                              msg.theta),
                      rospy.Time.now(),
                      turtlename,
                      "world")
@@ -16,7 +20,11 @@ def handle_turtle_pose(msg, turtlename):
 
 if __name__ == '__main__':
     rospy.init_node('turtle_tf_broadcaster')
+    # Get the name of the turtle from ROS.
+    # The value is specified in the launch file.?
     turtlename = rospy.get_param('~turtle')
+    # Subscribe to topic '/turtlename/pose' and
+    # run function 'handle_turtle_pose' on every incoming message.
     rospy.Subscriber('/%s/pose' % turtlename,
                      turtlesim.msg.Pose,
                      handle_turtle_pose,
